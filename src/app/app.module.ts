9b24c4a1cd752264a44e4480656b5a07b1a255ee
acerbastimur <umutacerbastimur@gmail.com>
2018-09-09 02:40:03 +0300

    head part was done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { Routes, RouterModule } from '@angular/router';
 
  import { CountUpModule } from 'countup.js-angular2';
  import {NgxPageScrollModule} from 'ngx-page-scroll';
-
+ 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent }]
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+     
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
-
-@Component({
+import { TweenMax,TimelineMax } from 'gsap'
+ 
+ @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
@@ -11,9 +12,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
-    $(window).scroll(() => {
-      let scroolPosition = $(window).scrollTop();
+    TweenMax.from('.headerText',1,{y:-100,opacity:1});
+    TweenMax.from('.welcomingText',1,{y:100,opacity:0,delay:1.2});
+    TweenMax.to('.btn-get-started',1,{opacity:1,delay:1.4});
+    TweenMax.to('.btn-get-started',0.6,{scale:1.3,delay:2,repeat:-1})
+     
+     $(window).scroll(() => {
+      const scroolPosition = $(window).scrollTop();
 
       if (scroolPosition >= 1000 && this.check) {
         console.log('GİRDİ');
@@ -28,12 +33,12 @@ export class HomeComponent implements OnInit {
                 $(this).text(Math.ceil(now));
               }
             });
-        }); 
-        this.check = false
+        });
+        this.check = false;
 
       }
 
-    })
+    });
   }
 
 }
